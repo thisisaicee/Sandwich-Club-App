1657a366bd3b9e5664d449838d393d35b9d2594d
Ifeanyichukwu Onyeabor <aicee@aicee.net>
2018-08-04 11:37:42 +0100

    updated DetailActivity.java to load contents accordingly

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,23 +4,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -34,21 +36,24 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
-        String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
-        if (sandwich == null) {
-            // Sandwich data unavailable
-            closeOnError();
-            return;
+        try{
+            String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
+            String json = sandwiches[position];
+
+            Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+            if (sandwich == null) {
+                // Sandwich data unavailable
+                closeOnError();
+                return;
+            }
+
+            loadDetails(sandwich);
+
+        }catch (Exception e){
+            e.printStackTrace();
         }
 
-        populateUI();
-        Picasso.with(this)
-                .load(sandwich.getImage())
-                .into(ingredientsIv);
 
-        setTitle(sandwich.getMainName());
     }
 
     private void closeOnError() {
@@ -56,7 +61,43 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    /**
+     * Display all Sandwich details
+     * @param sandwich
+     */
+    private void loadDetails(Sandwich sandwich) {
+        ImageView sandwichImage = findViewById(R.id.image);
+        TextView alsoKnownAs = findViewById(R.id.also_known_as_details);
+        TextView ingredients = findViewById(R.id.ingredients_details);
+        TextView origin = findViewById(R.id.origin_details);
+        TextView description = findViewById(R.id.description_details);
 
+        Picasso.with(this)
+                .load(sandwich.getImage())
+                .into(sandwichImage);
+
+        setTitle(sandwich.getMainName());
+        List<String> sandwichNameList = sandwich.getAlsoKnownAs();
+        for(int i=0 ; i< sandwichNameList.size() ; i++){
+
+            if(i + 1 < sandwichNameList.size()){
+                alsoKnownAs.append(" "+ sandwichNameList.get(i) + ",");
+            }else if (!(i + 1 < sandwichNameList.size())){
+                alsoKnownAs.append(" " + sandwichNameList.get(i) + ".");
+            }
+        }
+
+        List<String> sandwichIngredientsList = sandwich.getIngredients();
+        for(int i=0 ; i< sandwichIngredientsList.size() ; i++){
+            if(i + 1 < sandwichIngredientsList.size()){
+            ingredients.append(sandwichIngredientsList.get(i) + ", ");
+            }
+            else if (!(i + 1 < sandwichIngredientsList.size())){
+                ingredients.append(sandwichIngredientsList.get(i) + ". ");
+            }
+        }
+
+        origin.append(sandwich.getPlaceOfOrigin());
+        description.append(sandwich.getDescription());
     }
 }
